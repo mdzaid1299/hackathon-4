f02cdfcc332fcc5d0a77d9a5f1ae86b2ce0b949c
HP <mdzaid1299@gmail.com>
2022-09-06 14:03:49 +0530

    initial

---
+++ src/main/java/com/jap/ticketing/TicketImpl.java
@@ -0,0 +1,69 @@
+package com.jap.ticketing;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+public class TicketImpl {
+    public List<BusRoute> readFile(String filename) {
+        List<BusRoute> busRoutes = new ArrayList<>();
+        try {
+            FileReader fileReader = new FileReader(filename);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String data = bufferedReader.readLine();
+            while ((data = bufferedReader.readLine()) != null) {
+                String[] val = data.split(",");
+                String schedule_no = val[0];
+                String route_no = val[1];
+                int ticket_from_stop_id = Integer.parseInt(val[2]);
+                int ticket_from_stop_seq_no = Integer.parseInt(val[3]);
+                int ticket_till_stop_id = Integer.parseInt(val[4]);
+                int ticket_till_stop_seq_no = Integer.parseInt(val[5]);
+                String ticket_date = val[6];
+                String ticket_time = val[7];
+                int total_ticket_amount = Integer.parseInt(val[8]);
+                double travelled_KM = Double.parseDouble(val[9]);
+
+                busRoutes.add(new BusRoute(schedule_no, route_no, ticket_from_stop_id, ticket_from_stop_seq_no, ticket_till_stop_id, ticket_till_stop_seq_no, ticket_date, ticket_time, total_ticket_amount, travelled_KM));
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return busRoutes;
+    }
+
+    public List<BusRoute> sortDistance(List<BusRoute> ticketDistance, Distance distance) {
+        Collections.sort(ticketDistance, new Distance());
+        return ticketDistance;
+    }
+
+    public int calculateTotalCollections(List<BusRoute> price_of_ticket) {
+        Iterator<BusRoute> iterator = price_of_ticket.iterator();
+        int total_collection = 0;
+        while (iterator.hasNext()) {
+            total_collection = total_collection + iterator.next().getTotal_ticket_amount();
+        }
+        return total_collection;
+    }
+
+    public static void main(String[] args) {
+        TicketImpl ticket = new TicketImpl();
+        String filename = "sample.csv";
+
+        List<BusRoute> routeList = ticket.readFile(filename);
+        for (BusRoute value : routeList) {
+            System.out.println(value);
+        }
+        System.out.println("===================================================");
+        int output = ticket.calculateTotalCollections(routeList);
+        System.out.println("output = " + output);
+        System.out.println("===================================================");
+        ticket.sortDistance(routeList, new Distance());
+        for (BusRoute value : routeList) {
+            System.out.println(value);
+        }
+    }
+}
